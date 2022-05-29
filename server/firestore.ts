8c2058b4d117f148460585457f365e373dfa8c17
nico-mayer <nico.m94@icloud.com>
2022-05-29 20:48:45 +0200

    firestore test

--- server/firestore.ts
+++ server/firestore.ts
@@ -3,13 +3,13 @@ import { initializeApp, getApps, cert } from "firebase-admin/app"
 
 const apps = getApps()
 
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
+)
+
 if (!apps.length) {
   initializeApp({
-    credential: cert({
-      projectId: process.env.PROJECT_ID,
-      clientEmail: process.env.CLIENT_EMAIL,
-      privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    }),
+    credential: cert(serviceAccount),
   })
 }
 

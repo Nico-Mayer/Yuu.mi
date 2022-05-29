55ba6f4208592658e29d93778d0e251746b64a2e
nico-mayer <nico.m94@icloud.com>
2022-05-29 17:53:30 +0200

    admin sdk cert via env

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,6 @@
 
 <template>
   <main class="flex mx-auto max-w-7xl justify-center mt-80">
-    <h1 class="font-semibold text-5xl">Home</h1>
+    <h1 class="font-semibold text-5xl">Home...</h1>
   </main>
 </template>

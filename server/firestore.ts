98d64d1f0a11e1790592af5a37e96d479a10be13
nico-mayer <nico.m94@icloud.com>
2022-05-29 18:31:01 +0200

    vercel env test

--- server/firestore.ts
+++ server/firestore.ts
@@ -6,9 +6,9 @@ const apps = getApps()
 if (!apps.length) {
   initializeApp({
     credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      projectId: process.env.PROJECT_ID,
+      clientEmail: process.env.CLIENT_EMAIL,
+      privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
   })
 }

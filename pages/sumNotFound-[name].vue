4bd23cbb820dd75265a1536104220cf0943ea332
nico-mayer <nico.m94@icloud.com>
2022-06-14 14:48:09 +0200

    sum not found page added

--- pages/sumNotFound-[name].vue
+++ pages/sumNotFound-[name].vue
@@ -3,5 +3,10 @@ const route = useRoute()
 </script>
 
 <template>
-  <main>Summoner {{ route.params.name }} not found!</main>
+  <main class="flex max-w-7xl m-auto mt-80 items-center justify-center">
+    <h1 class="font-bold text-xl opacity-60">
+      Summoner <span class="text-gruvAquaM">{{ route.params.name }} </span> not
+      found!
+    </h1>
+  </main>
 </template>
